a8a44aee2e6f7ca48da305935d905799830bbe3c
Muhammad Aliif Nashrullah <110006007+NashAliif@users.noreply.github.com>
2025-01-08 17:31:49 +0700

    Delete app/config directory

--- app/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/kerja/SFI/Sistem%20Ajuan/Intranet');
-define('PUBLIC_PATH', 'http://localhost/kerja/SFI/Sistem%20Ajuan/Intranet/public');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'intranet_sfi');
